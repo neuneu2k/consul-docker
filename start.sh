4142b89302b898aa13edd3ef761eae641a4d3c5f
neuneu2k <github@neuneu2k.org>
2015-04-04 18:45:35 +0200

    Working version

--- start.sh
+++ start.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 # Wait for weave interface to go up
-ITERS="0";
-while [-n `ifconfig |grep weave` ]
+ITERS=0;
+while [[ `ifconfig |grep ethwe` = "" ]]
 do
-    sleep 0.1
-    ITERS$[$ITERS+1]
-    if [ $ITERS > 50 ]; then
-        echo "No weave network detected"
-        exit -1
-    fi
+    sleep 1
 done
 exec /consul $@
